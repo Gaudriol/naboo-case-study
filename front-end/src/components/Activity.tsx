75feadf05c6db81d3ac3da43384159fdfcbb08d4
Romain PETER <romain.peter3@gmail.com>
2025-06-15 17:52:24 +0200

    feat(front): add favorite button on activity card

--- front-end/src/components/Activity.tsx
+++ front-end/src/components/Activity.tsx
@@ -1,24 +1,84 @@
-import { ActivityFragment } from "@/graphql/generated/types";
+import {
+  ActivityFragment,
+  GetActivityQuery,
+  GetActivityQueryVariables,
+  ToggleActivityAsFavoriteMutation,
+  ToggleActivityAsFavoriteMutationVariables,
+} from "@/graphql/generated/types";
+import ToggleActivityAsFavorite from "@/graphql/mutations/user/toggleActivityAsFavorite";
+import GetActivity from "@/graphql/queries/activity/getActivity";
+import { useSnackbar } from "@/hooks";
 import { useGlobalStyles } from "@/utils";
-import { Badge, Button, Card, Grid, Group, Image, Text } from "@mantine/core";
+import { useMutation, useQuery } from "@apollo/client";
+import {
+  ActionIcon,
+  Badge,
+  Button,
+  Card,
+  Grid,
+  Group,
+  Image,
+  Text,
+} from "@mantine/core";
+import { IconHeartFilled, IconHeart } from "@tabler/icons-react";
 import Link from "next/link";
 
 interface ActivityProps {
   activity: ActivityFragment;
 }
 
-export function Activity({ activity }: ActivityProps) {
+export function Activity({ activity: initialActivity }: ActivityProps) {
   const { classes } = useGlobalStyles();
+  const snackbar = useSnackbar();
+
+  const { data } = useQuery<GetActivityQuery, GetActivityQueryVariables>(
+    GetActivity,
+    {
+      variables: { id: initialActivity.id },
+      fetchPolicy: "cache-and-network",
+    }
+  );
+
+  const activity = data?.getActivity || initialActivity;
+
+  const [toggleActivityAsFavorited] = useMutation<
+    ToggleActivityAsFavoriteMutation,
+    ToggleActivityAsFavoriteMutationVariables
+  >(ToggleActivityAsFavorite, {
+    refetchQueries: [
+      {
+        query: GetActivity,
+        variables: { id: activity.id },
+      },
+    ],
+    onError: () => {
+      snackbar.error("Une erreur est survenue.");
+    },
+  });
+
+  const onToggleFavorite = () => {
+    toggleActivityAsFavorited({
+      variables: { activityId: activity.id },
+    });
+  };
 
   return (
     <Grid.Col span={4}>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card shadow="sm" radius="md" style={{ position: "relative" }} withBorder>
         <Card.Section>
           <Image
             src="https://dummyimage.com/480x4:3"
             height={160}
             alt="random image of city"
           />
+          <ActionIcon
+            onClick={onToggleFavorite}
+            variant="transparent"
+            color="pink"
+            style={{ position: "absolute", top: 4, right: 4, zIndex: 1 }}
+          >
+            {activity.isFavorited ? <IconHeartFilled /> : <IconHeart />}
+          </ActionIcon>
         </Card.Section>
 
         <Group position="apart" mt="md" mb="xs">
